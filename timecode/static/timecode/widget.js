0cdbbb9ee2fa4d6b48bc9172d9472807e6025615
Joe Rickerby <joerick@mac.com>
2013-09-20 12:44:57 +0100

    Modified widget js behaviour

--- timecode/static/timecode/widget.js
+++ timecode/static/timecode/widget.js
@@ -1,3 +1,11 @@
+function isDigit(asciiCode) {
+    return asciiCode >= 48 && asciiCode < 58;
+}
+
+function isDivider(asciiCode) {
+    return asciiCode == 58 || asciiCode == 46;
+}
+
 $(document).ready(function () {
     var f = $(".timecode-widget");
     f.attr("placeholder", "hh:mm:ss:ff");
@@ -10,9 +18,17 @@ $(document).ready(function () {
             return;
         }
 
+        if (!isDigit(key)) {
+            event.preventDefault();
+        }
+
         if ($(this).caret() == valueLength) {
             // we are entering at the end of the string
             if (valueLength % 3 == 2) {
+                if (isDivider(key)) {
+                    event.preventDefault();
+                }
+
                 $(this).val($(this).val() + ":");
             }
         }
