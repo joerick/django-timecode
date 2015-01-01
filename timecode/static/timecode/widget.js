567af7c7dd6130eb9cea3c3f9065c0bc1d801df1
Tom Henderson <tomhenderson@mac.com>
2015-01-01 13:53:54 +1300

    Indentation.

--- timecode/static/timecode/widget.js
+++ timecode/static/timecode/widget.js
@@ -7,32 +7,32 @@ function isDivider(asciiCode) {
 }
 
 (function($) {
-$(document).ready(function () {
-    var f = $(".timecode-widget");
-    f.attr("placeholder", "hh:mm:ss:ff");
-    f.keypress(function (event) {
-        var key = event.which;
-        var valueLength = $(this).val().length;
+    $(document).ready(function () {
+        var f = $(".timecode-widget");
+        f.attr("placeholder", "hh:mm:ss:ff");
+        f.keypress(function (event) {
+            var key = event.which;
+            var valueLength = $(this).val().length;
 
-        if (valueLength >= 11) {
-            event.preventDefault();
-            return;
-        }
-
-        if (!isDigit(key)) {
-            event.preventDefault();
-        }
-
-        if ($(this).caret() == valueLength) {
-            // we are entering at the end of the string
-            if (valueLength % 3 == 2) {
-                if (isDivider(key)) {
-                    event.preventDefault();
-                }
-
-                $(this).val($(this).val() + ":");
+            if (valueLength >= 11) {
+                event.preventDefault();
+                return;
             }
-        }
+
+            if (!isDigit(key)) {
+                event.preventDefault();
+            }
+
+            if ($(this).caret() == valueLength) {
+                // we are entering at the end of the string
+                if (valueLength % 3 == 2) {
+                    if (isDivider(key)) {
+                        event.preventDefault();
+                    }
+
+                    $(this).val($(this).val() + ":");
+                }
+            }
+        });
     });
-});
 })(django.jQuery)
